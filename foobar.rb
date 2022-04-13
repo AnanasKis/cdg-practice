1d14753d32f6f5047a442bbe4536ee625e56771b
AnanasKis <100566909+AnanasKis@users.noreply.github.com>
2022-04-13 11:40:46 +0300

    Add files via upload

---
+++ foobar.rb
@@ -0,0 +1,10 @@
+def foobar 
+  print "Введите первое число"
+  first=gets.to_i
+  print "Введите второе число"
+  second=gets.to_i
+  if first ==20 || second==20
+    puts second 
+  else (puts first + second)
+  end 
+end
